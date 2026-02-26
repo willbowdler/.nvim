d17e3e9ddea592e9914606a758d4e7679ddc6a69
Will Bowdler <callowaybowdler@gmail.com>
2026-02-26 13:44:49 -0600

    Switch to omarchy config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,12 @@
-/home/basile/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({})
+      vim.cmd("colorscheme github_dark_high_contrast")
+    end,
+  },
+}
